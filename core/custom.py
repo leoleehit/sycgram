b92904cd6c56dfa873cdb55443d9adfeb08fe0be
iwumingz <iwuming@outlook.com>
2022-04-07 16:08:10 +0800

    Update and customize cmd and prefix in command.yml

--- core/custom.py
+++ core/custom.py
@@ -10,20 +10,25 @@
 """
 
 
-from typing import List, Union
+from typing import Any, Dict
+
+import yaml
 from pyrogram import filters
 from pyrogram.types import Message
 from tools.constants import STORE_TRACE_DATA
 from tools.storage import SimpleStore
 
+command_data: Dict[str, Any] = yaml.full_load(open("./data/command.yml", 'rb'))
 
-def command(command: Union[str, List[str]]):
+
+def command(key: str):
     """匹配UserBot指令"""
     # 指令前缀
     # 例一：prefixes = "-"
     # 例二：prefixes = ["-", "/", "+"]
-    prefixes = "-"
-    return filters.me & filters.text & filters.command(command, prefixes)
+    prefixes = command_data.get('help').get('all_prefixes')
+    cmd = command_data.get(key).get('cmd')
+    return filters.me & filters.text & filters.command(cmd, prefixes)
 
 
 def is_traced():
--- plugins/other.py
+++ plugins/other.py
@@ -1,5 +1,4 @@
 import asyncio
-import re
 
 from core import command
 from loguru import logger
@@ -10,18 +9,25 @@ from tools.helpers import delete_this, escape_markdown
 from tools.sessions import session
 
 
-@Client.on_message(command(['biss', 'diss', 'tg']))
-async def other(_: Client, msg: Message):
-    """喷人/舔狗"""
-    if bool(re.match(r"-(d|b)iss", msg.text)):
-        symbol = '💢 '
-        api = 'https://zuan.shabi.workers.dev/'
-    elif bool(re.match(r"-tg", msg.text)):
-        symbol = '👅 '
-        api = 'http://ovooa.com/API/tgrj/api.php'
-
+@Client.on_message(command("diss"))
+async def diss(_: Client, msg: Message):
+    """喷人"""
+    symbol = '💢 '
+    api = 'https://zuan.shabi.workers.dev/'
     await msg.edit_text(f"{symbol}It's preparating.")
+    await get_api(api=api, msg=msg)
 
+
+@Client.on_message(command('tg'))
+async def tg(_: Client, msg: Message):
+    """舔狗"""
+    symbol = '👅 '
+    api = 'http://ovooa.com/API/tgrj/api.php'
+    await msg.edit_text(f"{symbol}It's preparating.")
+    await get_api(api=api, msg=msg)
+
+
+async def get_api(api: str, msg: Message) -> None:
     for _ in range(10):
         try:
             resp = await session.get(api, timeout=5.5)
@@ -32,7 +38,6 @@ async def other(_: Client, msg: Message):
         except Exception as e:
             logger.error(e)
             continue
-
         words = f"{msg.reply_to_message.from_user.mention(style='md')} {text}" \
             if msg.reply_to_message else text
         try:
@@ -42,10 +47,8 @@ async def other(_: Client, msg: Message):
             await msg.edit_text(words, parse_mode='md')
         except RPCError as e:
             logger.error(e)
-
         await logger.complete()
         return
-
     # Failed to get api text
     await delete_this(msg)
     res = await msg.edit_text('😤 Rest for a while.')
